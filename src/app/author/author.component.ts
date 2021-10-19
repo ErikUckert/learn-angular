c3a8d41a0fe9b5660ba22dd7ef89ee79ea2b72d1
Erik Uckert <erik@Eriks-MacBook-Air.local>
2021-10-19 11:48:52 +0200

    add a component for displaying authors and a service for providing these authors

---
+++ src/app/author/author.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthorsService } from '../authors.service';
+
+@Component({
+  selector: 'app-author',
+  templateUrl: './author.component.html',
+  styleUrls: ['./author.component.css']
+})
+export class AuthorComponent implements OnInit {
+
+  authors;
+  constructor(service: AuthorsService) {
+    this.authors = service.getAuthors();
+  }
+
+  ngOnInit(): void {
+  }
+}
